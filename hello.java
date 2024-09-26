ddf61d951f5498561de6d042d985e0eaa50d1cfe
bindu.r <bindurkdh1998@gmail.com>
2024-09-26 17:51:59 +0530

    "comitting the changes version1"

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello {
+
+    public static void main(String args[]){
+        String text ="hello text";
+        test(text);
+    }
+
+    private static void test(String text) {
+        System.out.println(text);
+    }
+
+}
